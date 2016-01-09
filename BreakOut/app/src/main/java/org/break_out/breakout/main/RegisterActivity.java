5d5783d40b3a1e64c180853d5d824643ce59a04c
MDuehr <maximiliandh@gmail.com>
2016-01-09 23:33:02 +0100

    Add enhanced version of login screen and react to user inputs

--- BreakOut/app/src/main/java/org/break_out/breakout/main/RegisterActivity.java
+++ BreakOut/app/src/main/java/org/break_out/breakout/main/RegisterActivity.java
@@ -1,5 +1,6 @@
 package org.break_out.breakout.main;
 
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,13 +9,28 @@ import org.break_out.breakout.R;
 
 public class RegisterActivity extends AppCompatActivity {
     private FragmentManager _manager;
-
+    private FirstRegisterFragment _firstFragment;
+    private SecondRegisterFragment _secondFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        _manager = getFragmentManager();
+        _firstFragment = new FirstRegisterFragment();
+        _secondFragment = new SecondRegisterFragment();
     }
+
+
+    public static class FirstRegisterFragment extends Fragment {
+
+    }
+
+    public static class SecondRegisterFragment extends Fragment {
+
+    }
+
+
 
 
 }
--- BreakOut/app/src/main/java/org/break_out/breakout/main/StartActivity.java
+++ BreakOut/app/src/main/java/org/break_out/breakout/main/StartActivity.java
@@ -1,26 +1,29 @@
 package org.break_out.breakout.main;
 
-import android.app.ActionBar;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.break_out.breakout.R;
 
-import java.net.URL;
-
 public class StartActivity extends AppCompatActivity {
     private Context _context;
     private TextView _textView_about;
     private RelativeLayout _relativeLayout_container;
+    private RelativeLayout _relativeLayout_hintWrapper;
 
     private Button _button_login;
+    private Button _button_register;
+    private EditText _editText_email;
+    private EditText _editText_password;
 
 
     @Override
@@ -31,6 +34,73 @@ public class StartActivity extends AppCompatActivity {
 
         _textView_about = (TextView) findViewById(R.id.start_textView_about);
         _relativeLayout_container = (RelativeLayout) findViewById(R.id.start_relativeLayout_mainWrapper);
+        _relativeLayout_hintWrapper = (RelativeLayout) findViewById(R.id.start_relativeLayout_popupWrapper);
+        _button_login = (Button) findViewById(R.id.start_button_logIn);
+        _button_register = (Button) findViewById(R.id.start_button_register);
+        _editText_email = (EditText) findViewById(R.id.start_editText_email);
+        _editText_password = (EditText) findViewById(R.id.start_editText_password);
 
+        _editText_email.addTextChangedListener(new BOTextWatcher());
+        _editText_password.addTextChangedListener(new BOTextWatcher());
+
+        _button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logIn();
+            }
+        });
+    }
+
+    /**
+     * Method called to login
+     */
+    private void logIn() {
+        //LogIn method
+        //check if both fields are populated, show error otherwise
+        String input_email = _editText_email.getText().toString();
+        String input_password = _editText_password.getText().toString();
+        if(input_email.isEmpty() || input_password.isEmpty()) {
+            showHint();
+        }
+    }
+
+    private void register() {
+        Intent registerIntent = new Intent(this,RegisterActivity.class);
+        startActivity(registerIntent);
+    }
+
+    private void checkIfEnoughInput() {
+        String input_email = _editText_email.getText().toString();
+        String input_password = _editText_password.getText().toString();
+        if(!input_email.isEmpty()&&!input_password.isEmpty()) {
+            hideHint();
+        }
+    }
+
+    private void showHint() {
+        _relativeLayout_hintWrapper.setVisibility(View.VISIBLE);
+
+    }
+
+    private void hideHint() {
+        _relativeLayout_hintWrapper.setVisibility(View.INVISIBLE);
+    }
+
+    private class BOTextWatcher implements TextWatcher {
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            checkIfEnoughInput();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
     }
 }
