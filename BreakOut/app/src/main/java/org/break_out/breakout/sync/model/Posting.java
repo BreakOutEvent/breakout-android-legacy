3563420e65da09165e4d607547bfe6ffcef698e2
Tino <spiele.port@arcor.de>
2016-01-09 20:15:18 +0100

    Improve implementation of Posting.
    Implement first draft of an actual HTTP POST call for Postings.

--- BreakOut/app/src/main/java/org/break_out/breakout/sync/model/Posting.java
+++ BreakOut/app/src/main/java/org/break_out/breakout/sync/model/Posting.java
@@ -1,16 +1,40 @@
 package org.break_out.breakout.sync.model;
 
+import android.location.Location;
+import android.util.Log;
+
+import com.orm.dsl.Ignore;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 /**
  * Created by Tino on 14.12.2015.
  */
 
 public class Posting extends SyncEntity {
 
-    private String _text;
+    @Ignore
+    public static final String BASE_URL = "http://breakout-development.herokuapp.com";
+
+    @Ignore
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    private String _challengeId = "";
+    private long _createdTimestamp = 0L;
+    private Location _location = null;
+    @Ignore
+    private Long _sentTimestamp = null;
+    private String _text = "";
 
     // SugarORM needs an empty constructor
     public Posting() {
-
+        _createdTimestamp = System.currentTimeMillis();
     }
 
     public void setText(String text) {
@@ -21,21 +45,59 @@ public class Posting extends SyncEntity {
         return _text;
     }
 
+    private String toJSON() {
+        return "{" +
+            "\"challenge_id\": \"" + _challengeId + "\"," +
+            "\"created\": \"" + _createdTimestamp + "\"," +
+            "\"location\": {" +
+                "\"lat\": " + (_location != null ? (int)(_location.getLatitude()) : "0") + "," +
+                "\"lon\": " + (_location != null ? (int)(_location.getLongitude()) : "0") +
+            "}," +
+            "\"sent\": \"" + _sentTimestamp + "\"," +
+            "\"text\": \"" + _text + "\"" +
+        "}";
+    }
+
     @Override
     public boolean uploadToServer() {
-        // Currently randomly returns success or failure
-        return (Math.random() > 0.5);
+        boolean success = false;
+
+        // Get current timestamp as the sent timestamp
+        _sentTimestamp = System.currentTimeMillis();
+
+        RequestBody body = RequestBody.create(JSON, toJSON());
+        Request request = new Request.Builder()
+                .url(BASE_URL + "/test/post/")
+                .post(body)
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        Response response = null;
+        try {
+            response = client.newCall(request).execute();
+            if(response.code() == 201) {
+                success = true;
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+
+            success = false;
+        }
+
+        // The sent timestamp is only valid for one request
+        _sentTimestamp = null;
+
+        return success;
     }
 
     @Override
     public boolean updateOnServer() {
-        // TODO
-        return false;
+        // TODO: Currently randomly returns success or failure
+        return (Math.random() > 0.5);
     }
 
     @Override
     public boolean deleteOnServer() {
-        // Currently randomly returns success or failure
+        // TODO: Currently randomly returns success or failure
         return (Math.random() > 0.5);
     }
 
