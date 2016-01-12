bcdddd7db16bd01ec15cf00e1fdd871a5f24f574
Maximilian Dühr <drack1@gmx.net>
2016-01-12 01:31:06 +0100

    Implement base for new design
    The new design has no final outline yet, the known objects and
    structures are added as a base for future work

--- BreakOut/app/src/main/java/org/break_out/breakout/main/RegisterActivity.java
+++ BreakOut/app/src/main/java/org/break_out/breakout/main/RegisterActivity.java
@@ -1,89 +1,62 @@
 package org.break_out.breakout.main;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import org.break_out.breakout.R;
 
 public class RegisterActivity extends FragmentActivity {
     private static final String TAG = "RegisterActivity";
-    private FragmentManager _manager;
-    private RelativeLayout _relativeLayout_fragmentHolder;
-    private FirstRegisterFragment _firstFragment;
-    private SecondRegisterFragment _secondFragment;
-    private ViewPager _viewPager;
-    private BORegisterFragmentAdapter _adapter;
+    private EditText _editText_last;
+    private ScrollView _scrollView_credentials;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        _manager = getSupportFragmentManager();
-        _adapter = new BORegisterFragmentAdapter(_manager);
-        _relativeLayout_fragmentHolder = (RelativeLayout) findViewById(R.id.register_relativeLayout_fragmentHolder);
-        _viewPager = (ViewPager) findViewById(R.id.register_viewPager);
-        _viewPager.setAdapter(_adapter);
-        _firstFragment = new FirstRegisterFragment();
-        _secondFragment = new SecondRegisterFragment();
 
-        _viewPager.setCurrentItem(0);
-        Log.d(TAG,"onCreate fired");
+        _editText_last = (EditText) findViewById(R.id.register_f1_editText_emergencyNumber);
+        _scrollView_credentials = (ScrollView) findViewById(R.id.register_scrollView_credentials);
+
+        _editText_last.setOnEditorActionListener(new OnUserInputFinishedListener());
     }
 
-    private class BORegisterFragmentAdapter extends FragmentPagerAdapter {
-
-        public BORegisterFragmentAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            Log.d(TAG,"getItem called");
-            switch(position) {
-                case 0:
-                    return _firstFragment;
-                case 1:
-                    return _secondFragment;
-                default:
-                    return _firstFragment;
-            }
-        }
-
-        @Override
-        public int getCount() {
-            return 2;
-        }
+    private void scrollToEnd() {
+        _scrollView_credentials.fullScroll(ScrollView.FOCUS_DOWN);
     }
 
 
-    public static class FirstRegisterFragment extends Fragment {
-
+    //Detect if the user has entered the credential and scroll down to the end of the list if yes
+    private class OnUserInputFinishedListener implements TextView.OnEditorActionListener {
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View layout = inflater.inflate(R.layout.fragment_register1,container,false);
-            Log.d(TAG, "FirstFragment onCreateView");
-            return layout;
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+            hideKeyboard();
+            scrollToEnd();
+            return true;
+        }
+
+        private void hideKeyboard() {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(_editText_last.getWindowToken(), 0);
         }
     }
-
-    public static class SecondRegisterFragment extends Fragment {
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View layout = inflater.inflate(R.layout.fragment_register2,container,false);
-            return layout;
-        }
-    }
-
-
 
 
 }
