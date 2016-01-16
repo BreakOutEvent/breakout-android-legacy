64517d84396fadcb4b3e20b5c7aaf9bab88b37f5
Tino <spiele.port@arcor.de>
2016-01-16 21:02:45 +0100

    Prepare UserManager for upgrade of user
    Implement an upgrade method to upgrade the current user. The UserManager will open the corresponding Activity (e.g., the registration Activity) and check the result of the Activity. Also implemented a simple method to log out the current user.

--- BreakOut/app/src/main/java/org/break_out/breakout/manager/UserManager.java
+++ BreakOut/app/src/main/java/org/break_out/breakout/manager/UserManager.java
@@ -15,6 +15,13 @@ public class UserManager {
 
     private User _currUser = new User();
 
+    private UserUpgradeListener _listener = null;
+
+    public interface UserUpgradeListener {
+        public void upgradeSuccessful();
+        public void upgradeFailed();
+    }
+
     private UserManager(Context context) {
         _context = context;
 
@@ -47,6 +54,43 @@ public class UserManager {
     }
 
     /**
+     * Upgrades the current user to the given role.<br />
+     * This method will launch an Activity which will handle the
+     * registration of the user. If everything went fine and the user
+     * could be upgraded, this method call the listener's {@link UserUpgradeListener#upgradeSuccessful()}
+     * method. If anything went wrong (e.g., the user cancelled the registration process) or the user
+     * already own this or a higher role, the listener's {@link UserUpgradeListener#upgradeFailed()} method
+     * will be called.
+     *
+     * @param role The role the current user should be upgraded to (has to be higher than the current one)
+     * @param listener The listener for the result of the upgrade
+     */
+    public void upgradeCurrentUser(User.Role role, UserUpgradeListener listener) {
+        // No such upgrade is possible, if the user already has this or a higher role
+        if(_currUser.isAtLeast(role)) {
+            listener.upgradeFailed();
+            return;
+        }
+
+        _listener = listener;
+
+        // TODO: Start register Activity, get results and call listener
+        // Don't forget to save the new user to the prefs!
+    }
+
+    /**
+     * Logs out the current user.
+     * Afterwards, the current user will be an empty dummy user
+     * with the role {@code VISITOR}.<br />
+     * You should think about rebuilding the UI after logging out the
+     * user.
+     */
+    public void logOutCurrentUser() {
+        _currUser = new User();
+        _currUser.saveToPrefs(_context);
+    }
+
+    /**
      * Returns the currently logged in user.
      * If there is no user logged in, this method will return
      * an empty instance of {@link User} with the role {@code VISITOR}.
@@ -64,7 +108,7 @@ public class UserManager {
      *
      * @return The role of the currently logged in user
      */
-    public User.Role getCurrentUserRole() {
+    public User.Role getCurrentUsersRole() {
         return _currUser.getRole();
     }
 
