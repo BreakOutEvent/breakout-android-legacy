639254332e1a7a53e4e638167c0add50b22c57e2
Maximilian Duehr <maximiliandh@gmail.com>
2016-12-20 13:14:39 +0100

    delete unusd classes and set TODO for future changes

--- BreakOut/app/src/main/java/org/break_out/breakout/model/ToBeUploadedPostings.java
+++
@@ -1,9 +0,0 @@
-package org.break_out.breakout.model;
-
-import com.orm.SugarRecord;
-
-/**
- * Created by Maximilian Dühr on 24.04.2016.
- */
-public class ToBeUploadedPostings extends SugarRecord {
-}
